04a2c3c53f0a071f81df6b6bfb3e7d4e76f94761
umarabrar-git <itxomarabrar@gmail.com>
2026-09-24 22:40:23 +0500

    The navbar is imoroved little bit

--- static/js/home/navbar/mobile_menu.js
+++ static/js/home/navbar/mobile_menu.js
@@ -1,13 +1,9 @@
 const navbarShell = document.querySelector("[data-home-navbar-shell]");
 const navbarToggle = document.querySelector("[data-home-navbar-toggle]");
 const navbar = document.querySelector(".home-navbar");
-const mobileBreakpoint = 900;
 
 const isMobileNavbar = () => {
-  const isTouchDevice = window.matchMedia(
-    "(hover: none) and (pointer: coarse)",
-  ).matches;
-  return window.innerWidth < mobileBreakpoint || isTouchDevice;
+  return window.matchMedia("(hover: none) and (pointer: coarse)").matches;
 };
 
 const closeMobileMenu = () => {
@@ -44,17 +40,13 @@ if (navbar) {
   window.addEventListener("scroll", () => {
     const currentScrollPosition = window.scrollY;
 
-    // Determine scroll direction
     if (currentScrollPosition > lastScrollPosition) {
-      // Scrolling DOWN
       if (!isScrollingDown) {
         isScrollingDown = true;
         navbar.classList.add("navbar-hidden");
-        // Close mobile menu if open
         closeMobileMenu();
       }
     } else {
-      // Scrolling UP or at top
       if (isScrollingDown) {
         isScrollingDown = false;
         navbar.classList.remove("navbar-hidden");
